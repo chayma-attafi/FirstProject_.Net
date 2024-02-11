3d04d28d1ea3f37eed962c01a9bf593181fa28e9
chayma2 <chayma.attafi@esprit.tn>
2024-02-11 18:07:54 +0100

    overrideToString

---
+++ AM_Application_Core/Interface/IFlightMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using AM_Application_Core.Domain;
+
+namespace AM_Application_Core.Interface
+{
+	public interface IFlightMethods
+	{
+		public List<DateTime> GetFlightDates(string destination);
+    }
+}
+
---
+++ AM_Application_Core/Service/FlightMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using AM_Application_Core.Domain;
+using AM_Application_Core.Interface;
+
+namespace AM_Application_Core.Service
+{
+	public class FlightMethods : IFlightMethods
+    {
+        public List<Flight> Flights { get; set; } = new List<Flight>();
+
+        //1 rer methode
+
+        //   public List<DateTime> GetFlightDates(string destination)
+        //   {
+        //       List<DateTime> dateTimes = new List<DateTime>();
+        //       for(int i=0; i<Flights.Count;i++)
+        //       {
+        //           if (Flights[i].Destination == destination)
+        //           {
+        //              dateTimes.Add(Flights[i].FlightDate);
+        //               Console.WriteLine(Flights[i].FlightDate);
+        //          }
+        //       }
+
+        //       return dateTimes;
+        //  }
+
+
+        //ForEach
+
+
+
+        public List<DateTime> GetFlightDates(string destination)
+               {
+                   List<DateTime> dateTimes = new List<DateTime>();
+
+                   foreach(Flight f in Flights)
+                   {
+                       if (f.Destination == destination)
+                       {
+                          dateTimes.Add(f.FlightDate);
+                           Console.WriteLine(f.FlightDate);
+                      }
+                   }
+
+                   return dateTimes;
+              }
+
+
+        }
+    }
+
